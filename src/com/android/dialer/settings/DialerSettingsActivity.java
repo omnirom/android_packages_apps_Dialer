7e28262879e4a7929f38107fee9c7cd3b325cb34
maxwen <max.weninger@gmail.com>
2016-01-16 18:27:37 +0100

    Dialer: disable lookup settings until fixed
    Change-Id: If292f0c4b623a363d8a70565201c44ff23a5bee7

--- src/com/android/dialer/settings/DialerSettingsActivity.java
+++ src/com/android/dialer/settings/DialerSettingsActivity.java
@@ -54,11 +54,11 @@ public class DialerSettingsActivity extends PreferenceActivity {
         TelephonyManager telephonyManager =
                 (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
 
-        final Header lookupSettingsHeader = new Header();
+        /*final Header lookupSettingsHeader = new Header();
         lookupSettingsHeader.titleRes = R.string.lookup_settings_label;
         lookupSettingsHeader.summaryRes = R.string.lookup_settings_description;
         lookupSettingsHeader.fragment = LookupSettingsFragment.class.getName();
-        target.add(lookupSettingsHeader);
+        target.add(lookupSettingsHeader);*/
 
         // Only add the call settings header if the current user is the primary/owner user.
         if (isPrimaryUser()) {
